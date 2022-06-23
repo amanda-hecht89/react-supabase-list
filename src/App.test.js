bb58cae73784746418534245ea9f2e156a2ba08e
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-22 18:00:10 -0600

    test 2

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/beans/i);
+  const linkElement = screen.getByText(/fruit/i);
   expect(linkElement).toBeInTheDocument();
 });
